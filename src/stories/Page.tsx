aaf6396e9e76679fb6cc21585ca79226db17b1bd
pawk3k <ravliv.pavlobohdan@gmail.com>
2022-06-02 15:34:36 +0200

    build: change header to oleg

--- src/stories/Page.tsx
+++ src/stories/Page.tsx
@@ -20,7 +20,7 @@ export const Page: React.VFC = () => {
       />
 
       <section>
-        <h2>Pages in Storybook</h2>
+        <h2>Oleg</h2>
         <p>
           We recommend building UIs with a{' '}
           <a href="https://componentdriven.org" target="_blank" rel="noopener noreferrer">
