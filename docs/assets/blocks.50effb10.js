776081222b24311424d03f4c645ff910c1263886
pawk3k <ravliv.pavlobohdan@gmail.com>
2022-06-06 19:23:20 +0200

    build: update deploy

--- docs/assets/blocks.50effb10.js
+++
@@ -1,6 +0,0 @@
-import{j as o,d as t}from"./iframe.5af1824f.js";import"./index.894ce06f.js";import"./Props.3c5c22dc.js";import"./jsx-runtime.176622e1.js";const r=o(()=>{},t`
-    Importing from '@storybook/addon-docs/blocks' is deprecated, import directly from '@storybook/addon-docs' instead:
-    
-    https://github.com/storybookjs/storybook/blob/next/MIGRATION.md#deprecated-scoped-blocks-imports
-`);r();
-//# sourceMappingURL=blocks.50effb10.js.map
